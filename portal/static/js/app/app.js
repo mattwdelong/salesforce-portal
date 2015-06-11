739c295e618dc6139dd3e4d551a5bc3f79978959
James Jesudason <james.jesudason@gmail.com>
2015-06-11 23:20:05 +0100

    Change design of the people search and detail screen.

--- portal/static/js/app/app.js
+++ portal/static/js/app/app.js
@@ -6,8 +6,9 @@ App = Ember.Application.create({
 
 App.Router.map(function() {
     this.resource('index', { path: '/' });
-    this.resource('people', { path: '/people' });
-    this.resource('person', { path: '/person/:Id' });
+    this.resource('people', { path: '/people' }, function() {
+        this.resource('person', { path: '/:Id' });
+    });
     this.resource('contact', { path: '/contact'});
     this.resource('events', { path: '/events'});
     this.resource('event_kidswork', { path: '/event/:Id/kidswork' });
