7ebff59e11eaa2b6ec4e33ae96651d03d808da89
James Jesudason <james.jesudason@gmail.com>
2016-04-03 14:10:20 +0100

    Openshift integration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Life Church Portal',
+      version='1.0',
+      description='Life Church Salesforce Portal',
+      author='James Jesudason',
+      author_email='james.jesudason@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask>=0.10.1'],
+      )
---
+++ wsgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+IP = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+
+from portal import app as application
+
+
+if __name__ == '__main__':
+    application.run(IP, PORT)
